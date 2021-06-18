9218928f529ba5f50c0ed4589b3d17c2bfe887c2
Jonathan Marpaung <60574415+jonathanmarp@users.noreply.github.com>
2021-06-18 09:46:43 +0700

    Make script install deps
    install deps for Linux

---
+++ install.sh
@@ -0,0 +1,21 @@
+# Check Internet
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+        echo "Internet: True"
+		MACHINE_TYPE=`uname -m`
+		if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+			echo "Architecture: x64"
+			wget https://github.com/laferenorg/CppGame/releases/download/v1.1/libSDL2.a
+			wget https://github.com/laferenorg/CppGame/releases/download/v1.1/libSDL2_image.a
+			mv libSDL2.a libs/
+			mv libSDL2_image.a libs/
+		else
+			echo "Architecture: x32"
+			wget https://github.com/laferenorg/CppGame/releases/download/v1.0/libSDL2.a
+			wget https://github.com/laferenorg/CppGame/releases/download/v1.0/libSDL2_image.a
+			mv libSDL2.a libs/
+			mv libSDL2_image.a libs/
+		fi
+else
+        echo "Internet: False"
+fi
