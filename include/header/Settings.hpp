18ce83e7ce3c85b1e09e34b41dd5dc7b3001f9b1
Jonathan Marpaung <60574415+jonathanmarp@users.noreply.github.com>
2021-06-19 14:48:14 +0700

    Reback Include
    I just want to delete SDL2 include

---
+++ include/header/Settings.hpp
@@ -0,0 +1,9 @@
+#ifndef _SETTINGS_HPP_
+#define _SETTINGS_HPP_
+
+	#define SCREEN_WIDTH  800
+	#define SCREEN_HEIGHT SCREEN_WIDTH * 0.8
+	#define GRAVITY       0.75
+	#define TILE_SIZE     40
+
+#endif // _SETTINGS_HPP_
