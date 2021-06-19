48b3b1d0c715453b5a351020e93e3ddde3a6c839
Jonathan Marpaung <60574415+jonathanmarp@users.noreply.github.com>
2021-06-19 14:51:53 +0700

    Update for installing
    Adding install auto for include library

--- install.sh
+++ install.sh
@@ -7,12 +7,18 @@ if [[ $? -eq 0 ]]; then
 			echo "Architecture: x64"
 			wget https://github.com/laferenorg/CppGame/releases/download/v1.1/libSDL2.a
 			wget https://github.com/laferenorg/CppGame/releases/download/v1.1/libSDL2_image.a
+			https://github.com/laferenorg/CppGame/releases/download/v1.1/SDL2.zip
+			unzip SDL2.zip
+			mv SDL2 include/
 			mv libSDL2.a libs/
 			mv libSDL2_image.a libs/
 		else
 			echo "Architecture: x32"
 			wget https://github.com/laferenorg/CppGame/releases/download/v1.0/libSDL2.a
 			wget https://github.com/laferenorg/CppGame/releases/download/v1.0/libSDL2_image.a
+			https://github.com/laferenorg/CppGame/releases/download/v1.0/SDL2.zip
+			unzip SDL2.zip
+			mv SDL2 include/
 			mv libSDL2.a libs/
 			mv libSDL2_image.a libs/
 		fi
